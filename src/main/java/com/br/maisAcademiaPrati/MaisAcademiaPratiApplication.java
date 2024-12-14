607b2b94e546d24517e547fbf3baac5778a1f0c3
nglai <nglaikoin@hotmail.com>
2024-12-13 22:35:52 -0300

    adicionado postgres com docker e flyway

---
+++ src/main/java/com/br/maisAcademiaPrati/MaisAcademiaPratiApplication.java
@@ -0,0 +1,13 @@
+package com.br.maisAcademiaPrati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisAcademiaPratiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisAcademiaPratiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/maisAcademiaPrati/MaisAcademiaPratiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.maisAcademiaPrati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisAcademiaPratiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
